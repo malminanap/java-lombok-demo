035066dd969201d33b93cf95ebd2b8b083c4242f
malminanap <malminanap@gmail.com>
2025-07-25 07:03:52 +0000

    nuevos cambios

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.example.DTO.ClaseJava;
+
 import lombok.Data;
 
 @Data
@@ -9,5 +11,11 @@ public class App {
     public static void main(String[] args) {
         App app = new App("¡Hola desde Lombok!");
         System.out.println(app.getMessage());
+
+        ClaseJava claseJava = new ClaseJava();
+        claseJava.setNombre("Mario");
+        claseJava.setEdad(38);
+
+        System.out.println("Mi nombre es " + claseJava.getNombre() + " y tengo " + claseJava.getEdad());
     }
 }
---
+++ src/main/java/com/example/DTO/ClaseJava.java
@@ -0,0 +1,9 @@
+package com.example.DTO;
+
+import lombok.Data;
+
+@Data
+public class ClaseJava {
+    private String nombre;
+    private Integer edad;
+}
