a1ad60a8835a62041140df9429fc111efe7f92ea
malminanap <malminanap@gmail.com>
2025-07-25 09:33:39 +0000

    Pending changes exported from your codespace

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,16 +2,8 @@ package com.example;
 
 import com.example.DTO.ClaseJava;
 
-import lombok.Data;
-
-@Data
 public class App {
-    private final String message;
-
     public static void main(String[] args) {
-        App app = new App("¡Hola desde Lombok!");
-        System.out.println(app.getMessage());
-
         ClaseJava claseJava = new ClaseJava();
         claseJava.setNombre("Mario");
         claseJava.setEdad(38);
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,12 +2,18 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.DTO.ClaseJava;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
     void messageShouldNotBeNull() {
-        App app = new App("Test");
-        assertNotNull(app.getMessage());
+        ClaseJava claseJava = new ClaseJava();
+        claseJava.setNombre("Mario");
+        claseJava.setEdad(38);
+
+        System.out.println("Mi nombre es " + claseJava.getNombre() + " y tengo " + claseJava.getEdad());
+        assertNotNull(claseJava.getNombre());
     }
 }
